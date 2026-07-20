89a049d591f324d102d1902a52a1c3529fd66466
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,4 @@
+return {
+    ["Describe words without saying the forbidden ones — team buzzer party game."] = { fr = "Décrivez des mots sans dire les mots interdits — jeu de groupe en équipes avec buzzer." },
+    ["Taboo Party"] = { fr = "Taboo Party" },
+}
